80d00fda90f0570c2c4efcf98e59ef51d33f1d07
Håkon Schia <hakon.schia@nrk.no>
2023-07-27 10:08:27 +0200

    Migrate to Github packages

--- native-bridge-sample/src/main/java/no/nrk/nativebridge/sample/WebViewActivity.kt
+++ native-bridge-sample/src/main/java/no/nrk/nativebridge/sample/WebViewActivity.kt
@@ -2,13 +2,14 @@ package no.nrk.nativebridge.sample
 
 import android.content.SharedPreferences
 import android.os.Build
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView.setWebContentsDebuggingEnabled
 import android.webkit.WebViewClient
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import android.widget.EditText
+import no.nrk.nativebridge.NativeBridgeWebView
 import no.nrk.nativebridge.sample.topicdata.GaConfTopicData
-
 
 class WebViewActivity : AppCompatActivity() {
 
@@ -19,10 +20,14 @@ class WebViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         prefs = getPreferences(android.content.Context.MODE_PRIVATE)
 
+        val etUrl: EditText = findViewById(R.id.etUrl)
+        val webview: NativeBridgeWebView = findViewById(R.id.webview)
+        val btnSetUrl: Button = findViewById(R.id.btnSetUrl)
+
         val url = prefs.getString("url", "http://10.0.2.2:8080")
         etUrl.setText(url)
 
-        webview?.apply {
+        webview.apply {
             connection.addHandler("gaConf", { _ : GaConfTopicData.In, connection ->
                     val gaConf = GaConfTopicData.Out("35009a79-1a05-49d7-b876-2b884d0f825b")
                     connection.send("gaConf", gaConf)
@@ -34,7 +39,9 @@ class WebViewActivity : AppCompatActivity() {
             }
 
             webViewClient = WebViewClient()
-            loadUrl(url)
+            if (url != null) {
+                loadUrl(url)
+            }
         }
 
         btnSetUrl.setOnClickListener {
